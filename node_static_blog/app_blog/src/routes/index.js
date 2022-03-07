1a32ca038ba7c237287c17650f01f4bc3023fcba
yujinhong01 <yujinhong01@58.com>
2022-03-07 16:44:10 +0800

    feat: 路由懒加载

--- node_static_blog/app_blog/src/routes/index.js
+++ node_static_blog/app_blog/src/routes/index.js
@@ -1,13 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-// import routes from "./routes";
-import Home from '../pages/home';
-import About from '../pages/about';
-import Article from '../pages/article';
-import ErrorPage from '../pages/error';
 import Nav from '../components/nav';
-import ArticleList from '../pages/article/components/articleList';
-import ArticleInfo from "../pages/article/components/articleInfo";
+import Loading from "../components/loading";
+
+// 懒加载
+const lazyLoad = (path) => {
+    // 这个方法需要注意的一点，这个 path 不能直接传`相对路径`类似 `../pages/App`, 会找不到组件，所以最好只传过来一个组件名字，然后内部拼接相对路径
+    const Comp = React.lazy(() => import(`../pages/${path}`))
+    return (
+        <React.Suspense fallback={<Loading />}>
+            <Comp />
+        </React.Suspense>
+    )
+}
+
 
 const ROUTE_PREFIX = '/yujinhong';
 // In react-router-dom v6, "Switch" is replaced by routes "Routes". 
@@ -16,16 +22,17 @@ function AppRouter () {
     return <Router basename={`${ROUTE_PREFIX}`}>
         <Nav />
         <Routes>
-            <Route path="/" element={<Home />}></Route>
-            <Route path="/about" element={<About />}></Route>
-            <Route path="/article" element={<Article />}>
-                <Route path="/article" element={<ArticleList />} />
-                <Route path="/article/:slug" element={<ArticleInfo />} />
+            <Route path="/" element={lazyLoad('home')}></Route>
+            <Route path="/about" element={lazyLoad('about')}></Route>
+            <Route path="/article" element={lazyLoad('article')}>
+                <Route path="/article" element={lazyLoad('article/components/articleList')} />
+                <Route path="/article/:slug" element={lazyLoad('article/components/articleInfo')} />
             </Route>
-            <Route path="/error" element={<ErrorPage />}></Route>
+            <Route path="/error" element={lazyLoad('home')}></Route>
             <Route path="*" element={<Navigate to="/error" />} />
         </Routes>
     </Router>
 }
+
 
 export default AppRouter;
